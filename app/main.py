ebf39afd991a5491300a508c0f9bb856d151c2b6
Naman Sharma <naman99sha@gmail.com>
2021-10-07 20:37:07 +0530

    Learning Jinja and fastAPI integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+# To return response in terms of HTML strings
+from fastapi.responses import HTMLResponse
+# For setting up templates in fastAPI
+from fastapi.templating import Jinja2Templates
+import pathlib  # You can also use OS library
 
 app = FastAPI()
 
+BASE_DIR = pathlib.Path(__file__).parent
+templates = Jinja2Templates(directory=str(BASE_DIR/'templates'))
 
-@app.get("/")
-def home_view():
+
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    print(request)
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.post("/")
+def home_detail_view():
     return {"hello": "world"}

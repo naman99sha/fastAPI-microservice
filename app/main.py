2836179c60dc26612ef73236a06ae38332e262c5
Naman Sharma <naman99sha@gmail.com>
2021-10-08 20:05:40 +0530

    Added upload view and updated requirements

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Request,
+    Depends,
+    File,
+    UploadFile
+)
 # To return response in terms of HTML strings
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 # For setting up templates in fastAPI
 from fastapi.templating import Jinja2Templates
 import pathlib  # You can also use OS library
 import os
 from pydantic import BaseSettings
 from functools import lru_cache
+import io
+import uuid
 
 
 class Settings(BaseSettings):
     debug: bool = False
+    echo_active: bool = False
 
     class Config:
         env_file = ".env"
@@ -26,6 +36,7 @@ DEBUG = get_settings().debug
 app = FastAPI()
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / 'uploads'
 templates = Jinja2Templates(directory=str(BASE_DIR/'templates'))
 
 
@@ -37,3 +48,17 @@ def home_view(request: Request, settings: Settings = Depends(get_settings)):
 @app.post("/")
 def home_detail_view():
     return {"hello": "world"}
+
+
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(file: UploadFile = File(...), settings: Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="Invalid Endpoint", status=400)
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix  # .jpg , .txt
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
+    # save the file uploaded
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    return dest

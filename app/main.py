777ec3a10d4505ca6841c4947c9913ef0beb1224
Naman Sharma <naman99sha@gmail.com>
2021-10-09 00:20:18 +0530

    Added authorization functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import (
     FastAPI,
+    Header,
     HTTPException,
     Request,
     Depends,
@@ -23,6 +24,9 @@ from PIL import Image
 class Settings(BaseSettings):
     debug: bool = False
     echo_active: bool = False
+    app_auth_token: str
+    app_auth_token_prod: str = None
+    skip_auth: bool = False
 
     class Config:
         env_file = ".env"
@@ -47,11 +51,19 @@ def home_view(request: Request, settings: Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
+def verify_auth(authorization=Header(None), settings: Settings = Depends(get_settings)):
+    if settings.debug and settings.skip_auth:
+        return
+    if authorization is None:
+        raise HTTPException(detail="Invalid Access", status_code=401)
+    label, token = authorization.split()
+    if token != settings.app_auth_token:
+        raise HTTPException(detail="Invalid Access", status_code=401)
+
+
 @app.post("/")
-async def prediction_view(file: UploadFile = File(...), settings: Settings = Depends(get_settings)):
-    if not settings.echo_active:
-        raise HTTPException(detail="Invalid Endpoint", status=400)
-    UPLOAD_DIR.mkdir(exist_ok=True)
+async def prediction_view(file: UploadFile = File(...), authorization=Header(None), settings: Settings = Depends(get_settings)):
+    verify_auth(authorization, settings)
     bytes_str = io.BytesIO(await file.read())
     try:
         img = Image.open(bytes_str)
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,6 +1,6 @@
 import shutil
 import time
-from app.main import app, BASE_DIR, UPLOAD_DIR
+from app.main import app, BASE_DIR, UPLOAD_DIR, get_settings
 from fastapi.testclient import TestClient
 from PIL import Image, ImageChops
 import io
@@ -14,14 +14,32 @@ def test_get_home():
     assert 'text/html' in response.headers["content-type"]
 
 
-def test_prediction_upload():
+def test_prediction_upload_header_missing():
     saved_image_path = BASE_DIR / "images"
+    settings = get_settings()
     for path in saved_image_path.glob("*"):
         try:
             img = Image.open(path)
         except:
             img = None
-        response = client.post("/", files={"file": open(path, 'rb')})
+        response = client.post("/",
+                               files={"file": open(path, 'rb')})
+        assert response.status_code == 401
+
+
+def test_prediction_upload():
+    saved_image_path = BASE_DIR / "images"
+    settings = get_settings()
+    for path in saved_image_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/",
+                               files={"file": open(path, 'rb')},
+                               headers={
+                                   "Authorization": f"JWT {settings.app_auth_token}"}
+                               )
         if img is not None:
             assert response.status_code == 200
             data = response.json()
